5511d42067f8e49216bfebfe938775a6993d6b82
Alek90 <4lek.90@gmail.com>
2021-10-08 15:50:22 +0200

    Cambiato nome variabile.
    Cambiato nome all'unica variabile al momento inserita.

--- main.js
+++ main.js
@@ -2,8 +2,8 @@
 
 // Chiedere nome
 
-const name = prompt ("Qual'è il tuo nome?")
-console.log (name)
+const first_name = prompt ("Qual'è il tuo nome?")
+console.log (first_name)
 
 // Chiedere cognome
 
