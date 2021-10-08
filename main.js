0de903ca8d1e531350c7bc91cbf2875021884b5e
Alek90 <4lek.90@gmail.com>
2021-10-08 15:58:03 +0200

    Primo passaggio effettuato.
    Testato su liveserver, risulta funzionante.

--- main.js
+++ main.js
@@ -5,6 +5,10 @@
 const first_name = prompt ("Qual'è il tuo nome?")
 console.log (first_name)
 
+// Mostrare risposta
+
+document.getElementById("name").innerHTML=first_name
+
 // Chiedere cognome
 
 // Chiedere colore preferito
